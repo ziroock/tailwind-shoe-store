08012ed5244405f68c6bbd5b1a9e0ec88ffaa86e
ziroock <zaprin@collegepulse.com>
2024-03-08 10:22:25 -0800

    working on nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 import nikeLogo from '@/assets/nike-logo.svg'
 import { RxHamburgerMenu } from 'react-icons/rx'
+import { TbShoppingBag } from 'react-icons/tb'
 
 const ROUTES = ['Home', 'About', 'Pricing', 'Contact']
 
@@ -9,7 +10,7 @@ export default function Nav() {
   return (
     <nav className="flex flex-wrap items-center justify-between">
       <Image className="h-20 w-20" src={nikeLogo} alt="Nike Logo" />
-      <button className="rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
+      <button className="rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 lg:hidden">
         <RxHamburgerMenu size={25} />
       </button>
       <div className="w-full">
@@ -23,6 +24,11 @@ export default function Nav() {
             </li>
           ))}
         </ul>
+      </div>
+      <div className="fixed bottom-4 left-4 lg:static">
+        <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+          <TbShoppingBag />
+        </div>
       </div>
     </nav>
   )
